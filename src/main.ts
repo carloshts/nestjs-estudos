faeb7793ebce16f28481a0833850035b639d8ab3
carloshts <henriquecarlos9@hotmail.com>
2022-09-02 18:51:22 -0300

    Testando Cors dnv

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ origin: /.+/ });
   const config = new DocumentBuilder()
     .setTitle('NestJS estudos')
     .setDescription('Api Rest de estudos em NestJS')
@@ -12,25 +12,19 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
+  // Habilita o CORS
   // app.use((req, res, next) => {
   //   res.header('Access-Control-Allow-Origin', '*');
-  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   res.header(
+  //     'Access-Control-Allow-Headers',
+  //     'Origin, X-Requested-With, Content-Type, Accept, x-access-token',
+  //   );
+  //   res.header(
+  //     'Access-Control-Allow-Methods',
+  //     'GET, POST, PUT, DELETE, OPTIONS',
+  //   );
   //   next();
   // });
-  // Habilita o CORS
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, x-access-token',
-    );
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, DELETE, OPTIONS',
-    );
-    next();
-  });
   await app.listen(process.env.PORT || 3000);
   //Forçando build dnv
 }
